96a4302ba1b8e1c1237836a7a99618dd18f2c7cb
Yannik Marchand <ymarchand@me.com>
2026-08-10 09:36:42 +0200

    Fix inconsistent use of tabs and spaces in setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
+
 import os
 import setuptools
 
 description = \
-	"ZBIC compression algorithm of Nintendo Switch."
+    "ZBIC compression algorithm of Nintendo Switch."
 
 if os.environ.get("GITHUB_WORKSPACE"):
-  # This is a workaround for the GitHub actions build on Windows.
-  # Relative paths aren't working.
-	this_directory = os.environ["GITHUB_WORKSPACE"]
+    # This is a workaround for the GitHub actions build on Windows.
+    # Relative paths aren't working.
+    this_directory = os.environ["GITHUB_WORKSPACE"]
 else:
-  this_directory = os.path.dirname(__file__)
+    this_directory = os.path.dirname(__file__)
 
 with open(os.path.join(this_directory, "README.md"), "r") as file:
-	long_description = file.read()
+    long_description = file.read()
 
 extensions = [setuptools.Extension(
-	name = "zbic",
-	sources = [os.path.join(this_directory, "src", "module.c"), os.path.join(this_directory, "src", "zstd.c")],
+    name = "zbic",
+    sources = [os.path.join(this_directory, "src", "module.c"), os.path.join(this_directory, "src", "zstd.c")],
     include_dirs = [os.path.join(this_directory, "src")],
-	define_macros = [("ZSTD_ZBIC_SUPPORT", "1")]
+    define_macros = [("ZSTD_ZBIC_SUPPORT", "1")]
 )]
 
 setuptools.setup(
-	name = "zbic",
-	version = "1.0.0.post1",
-	description = description,
-	long_description = long_description,
+    name = "zbic",
+    version = "1.0.0.post1",
+    description = description,
+    long_description = long_description,
     long_description_content_type = "text/markdown",
-	author = "Yannik Marchand",
-	author_email = "ymarchand@me.com",
-	url = "https://github.com/kinnay/zbic",
-	license = "GPLv2",
-	ext_modules = extensions
+    author = "Yannik Marchand",
+    author_email = "ymarchand@me.com",
+    url = "https://github.com/kinnay/zbic",
+    license = "GPLv2",
+    ext_modules = extensions
 )
