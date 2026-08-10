1cfd4baf1eb1a293640212a8b7fd7248abecb227
TSRBerry <20988865+TSRBerry@users.noreply.github.com>
2026-08-10 09:34:43 +0200

    Add GitHub workflows to publish to PyPi (wheels + sdist) (#3)

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ import setuptools
 description = \
 	"ZBIC compression algorithm of Nintendo Switch."
 
-this_directory = os.path.dirname(__file__)
+if os.environ.get("GITHUB_WORKSPACE"):
+  # This is a workaround for the GitHub actions build on Windows.
+  # Relative paths aren't working.
+	this_directory = os.environ["GITHUB_WORKSPACE"]
+else:
+  this_directory = os.path.dirname(__file__)
+
 with open(os.path.join(this_directory, "README.md"), "r") as file:
 	long_description = file.read()
 
 extensions = [setuptools.Extension(
 	name = "zbic",
-	sources = ["src/module.c", "src/zstd.c"],
-    include_dirs = ["src"],
+	sources = [os.path.join(this_directory, "src", "module.c"), os.path.join(this_directory, "src", "zstd.c")],
+    include_dirs = [os.path.join(this_directory, "src")],
 	define_macros = [("ZSTD_ZBIC_SUPPORT", "1")]
 )]
 
